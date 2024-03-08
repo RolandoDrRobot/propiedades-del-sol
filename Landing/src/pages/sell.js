97f5367e3aeec9b6364a9a67cf5e42fa2fffc9a4
RolandoDrRobot <rolando.arguello.email@gmail.com>
2024-03-08 13:49:17 -0600

    More spanish changes

--- Landing/src/pages/sell.js
+++ Landing/src/pages/sell.js
@@ -9,49 +9,49 @@ import GetInTuch from "../component/Get-in-tuch";
 
 export default function Sell() {
 
-    // useEffect(() => {
-    //     const rangeSlider = document.getElementById('slider');
-    //     const value = rangeSlider.value;
-    //     document.getElementById('amount-label').innerHTML = value;
-    //     document.getElementById('saving-label').innerHTML = parseFloat(value * 0.01).toFixed(2);
-    //     window.scrollTo(0, 0)
-    // });
+  // useEffect(() => {
+  //     const rangeSlider = document.getElementById('slider');
+  //     const value = rangeSlider.value;
+  //     document.getElementById('amount-label').innerHTML = value;
+  //     document.getElementById('saving-label').innerHTML = parseFloat(value * 0.01).toFixed(2);
+  //     window.scrollTo(0, 0)
+  // });
 
-    // const handleChange = (e) => {
-    //     console.log(e.target.value, '32');
-    //     const value = e.target.value;
-    //     document.getElementById('amount-label').innerHTML = value;
-    //     document.getElementById('saving-label').innerHTML = parseFloat(value * 0.01).toFixed(2);
-    // }
+  // const handleChange = (e) => {
+  //     console.log(e.target.value, '32');
+  //     const value = e.target.value;
+  //     document.getElementById('amount-label').innerHTML = value;
+  //     document.getElementById('saving-label').innerHTML = parseFloat(value * 0.01).toFixed(2);
+  // }
 
 
-    return (
-        <>
-            <Navbar navClass="navbar-white" />
-            {/* <!-- Start Hero --> */}
+  return (
+    <>
+      <Navbar navClass="navbar-white" />
+      {/* <!-- Start Hero --> */}
 
-            <section
-                style={{ backgroundImage: `url(${BackgroundImage})` }}
-                className="relative table w-full py-32 lg:py-36 bg-[url('../../assets/images/bg/01.jpg')] bg-no-repeat bg-center bg-cover">
-                <div className="absolute inset-0 bg-black opacity-80"></div>
-                <div className="container">
-                    <div className="grid grid-cols-1 text-center mt-10">
-                        <h3 className="md:text-4xl text-3xl md:leading-normal leading-normal font-medium text-white">Sell Faster. Save Thousands.</h3>
-                    </div>
-                </div>
-            </section>
-            <div className="relative">
-                <div className="shape overflow-hidden z-1 text-white dark:text-slate-900">
-                    <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
-                    </svg>
-                </div>
-            </div>
+      <section
+        style={{ backgroundImage: `url(${BackgroundImage})` }}
+        className="relative table w-full py-32 lg:py-36 bg-[url('../../assets/images/bg/01.jpg')] bg-no-repeat bg-center bg-cover">
+        <div className="absolute inset-0 bg-black opacity-80"></div>
+        <div className="container">
+          <div className="grid grid-cols-1 text-center mt-10">
+            <h3 className="md:text-4xl text-3xl md:leading-normal leading-normal font-medium text-white">Vende rápido. Ahorra miles</h3>
+          </div>
+        </div>
+      </section>
+      <div className="relative">
+        <div className="shape overflow-hidden z-1 text-white dark:text-slate-900">
+          <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
+          </svg>
+        </div>
+      </div>
 
-            {/* End Hero  */}
-            <section className="relative md:pb-24 pb-16">
-                <Feature />
-                {/* <div className="container lg:mt-24 mt-16">
+      {/* End Hero  */}
+      <section className="relative md:pb-24 pb-16">
+        <Feature />
+        {/* <div className="container lg:mt-24 mt-16">
                     <div className="grid grid-cols-1 pb-8 text-center">
                         <h3 className="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">Brokerage Calculator</h3>
 
@@ -93,11 +93,11 @@ export default function Sell() {
                     </div>
                 </div> */}
 
-               <GetInTuch/>
-            </section>
-            <Footer />
-            <Switcher />
-        </>
-    );
+        {/* <GetInTuch/> */}
+      </section>
+      <Footer />
+      <Switcher />
+    </>
+  );
 
 }
